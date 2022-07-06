4682a0be5df73a34dc125ed91f06d72743e44ae0
ENES DEDEOĞLU <101185956+enesdedeoglu0@users.noreply.github.com>
2022-07-07 00:38:22 +0300

    Add files via upload

---
+++ POINTER SWAP ISLEMI/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void terstenYaz(int *dizi, int n);
+int main() {
+    int dizi [] = {10 ,20,30,40,50,60,70};
+    int diziElemanSayisi = sizeof(dizi)/sizeof(int);
+    int *dizi_ptr;
+    dizi_ptr = dizi;
+    for (int i= 0;i<diziElemanSayisi;i++){
+       // printf("%d\t",dizi[i]);
+        printf("%d \t",*(dizi_ptr+i));
+    }
+
+
+    terstenYaz(dizi,diziElemanSayisi);
+    printf("\n");
+    for (int i= 0;i<diziElemanSayisi;i++){
+        // printf("%d\t",dizi[i]);
+        printf("%d \t",*(dizi_ptr+i));
+    }
+    return 0;
+}
+void terstenYaz(int *dizi, int n){
+    int gecici;
+
+    for (int i = 0;i < n/2;i++){
+        gecici = *(dizi+i);
+        *(dizi+i)=*(dizi+(n-1)-i);
+        *(dizi+(n-1)-i)= gecici;
+    }
+
+
+}
